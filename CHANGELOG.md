7c84fcbd98275e9b5f0c2d321ade3d21cdd19ec6
Drew M <39811322+FlyingKiwis@users.noreply.github.com>
2023-10-21 14:58:27 -0400

    Initial

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+﻿# Point3D
+
+## 1.0.0.1
+- Initial release
---
+++ Point3D/Point3d.cs
@@ -0,0 +1,69 @@
+﻿using NINA.Point3d.Properties;
+using NINA.Core.Model;
+using NINA.Core.Utility;
+using NINA.Image.ImageData;
+using NINA.Plugin;
+using NINA.Plugin.Interfaces;
+using NINA.Profile;
+using NINA.Profile.Interfaces;
+using NINA.WPF.Base.Interfaces.Mediator;
+using NINA.WPF.Base.Interfaces.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Settings = NINA.Point3d.Properties.Settings;
+
+namespace NINA.Point3d {
+    /// <summary>
+    /// This class exports the IPluginManifest interface and will be used for the general plugin information and options
+    /// The base class "PluginBase" will populate all the necessary Manifest Meta Data out of the AssemblyInfo attributes. Please fill these accoringly
+    /// 
+    /// An instance of this class will be created and set as datacontext on the plugin options tab in N.I.N.A. to be able to configure global plugin settings
+    /// The user interface for the settings will be defined by a DataTemplate with the key having the naming convention "Point3d_Options" where Point3d corresponds to the AssemblyTitle - In this template example it is found in the Options.xaml
+    /// </summary>
+    [Export(typeof(IPluginManifest))]
+    public class Point3d : PluginBase, INotifyPropertyChanged {
+        private readonly IPluginOptionsAccessor pluginSettings;
+        private readonly IProfileService profileService;
+        private readonly IImageSaveMediator imageSaveMediator;
+
+        [ImportingConstructor]
+        public Point3d(IProfileService profileService, IOptionsVM options, IImageSaveMediator imageSaveMediator) {
+            if (Settings.Default.UpdateSettings) {
+                Settings.Default.Upgrade();
+                Settings.Default.UpdateSettings = false;
+                CoreUtil.SaveSettings(Settings.Default);
+            }
+
+            // This helper class can be used to store plugin settings that are dependent on the current profile
+            this.pluginSettings = new PluginOptionsAccessor(profileService, Guid.Parse(this.Identifier));
+            this.profileService = profileService;
+            // React on a changed profile
+            profileService.ProfileChanged += ProfileService_ProfileChanged;
+
+            // Hook into image saving for adding FITS keywords or image file patterns
+            this.imageSaveMediator = imageSaveMediator;
+        }
+
+        public override Task Teardown() {
+            // Make sure to unregister an event when the object is no longer in use. Otherwise garbage collection will be prevented.
+            profileService.ProfileChanged -= ProfileService_ProfileChanged;
+
+            return base.Teardown();
+        }
+
+        private void ProfileService_ProfileChanged(object sender, EventArgs e) {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null) {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
